d458dea8db7e3d9d70b4a0c855b52aa53f289980
Daniel Clements <drclements@liberty.edu>
2022-10-31 21:07:44 -0400

    commit tree 52908aa640024b05a5bdebd1a47140a139c95b5c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (event = "go to the office") {
+    return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective (character = "*") {
+    return function (adjective = "special") {
+        return `You are ${character}${adjective}${character}!`
+    }
+}
+
